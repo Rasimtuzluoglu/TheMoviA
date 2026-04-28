72fdd529e45cbe45b09ef0d92d17d64610ad78b2
Rasim Tuzluoglu <145382852+Rasimtuzluoglu@users.noreply.github.com>
2026-04-28 21:06:22 +0300

    first spbtprjct

---
+++ src/main/java/com/raspel/movia/MoviaApplication.java
@@ -0,0 +1,13 @@
+package com.raspel.movia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raspel/movia/MoviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raspel.movia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
